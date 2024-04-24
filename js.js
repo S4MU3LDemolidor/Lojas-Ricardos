f555dc4a1ebd19b1503e470cdff8d20fcda65da6
Samuel Ribeiro Cardoso <143662788+S4MU3LDemolidor@users.noreply.github.com>
2024-04-24 14:20:36 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+function scrollToElement(id) {
+    var element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth' });
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
